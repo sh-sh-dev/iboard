f0009557123e1e9ae6c4707b6a21ab77d33004ef
Shaygan <shayganshok0@gmail.com>
2023-08-19 01:22:52 +0330

    feat(orders): responsive table

--- resources/views/orders.blade.php
+++ resources/views/orders.blade.php
@@ -16,29 +16,31 @@
                 <span>({{ $orders->count() }})</span>
             </h3>
             <hr>
-            <table class="table p-5 table-dark text-center table-hover">
-            <thead>
-            <tr>
-                <th scope="col" width="1">#</th>
-                <th scope="col">محصول</th>
-                <th scope="col">قطعه آماده</th>
-                <th scope="col">قیمت</th>
-                <th scope="col">تاریخ</th>
-                <th scope="col"></th>
-            </tr>
-            </thead>
-            <tbody>
-            @foreach($orders as $order)
-                <tr>
-                    <th scope="row" title="{{ $order->id }}">{{ $loop->iteration }}</th>
-                    <td>{{ $order->humanized_product }}</td>
-                    <td>{{ $order->type ? 'بله' : '-' }}</td>
-                    <td>{{ $order->humanized_price }}</td>
-                    <td>{{ $order->jalali_date }}</td>
-                    <td><a class="btn btn-outline-danger btn-sm delete" href="{{ route('orders.delete', $order) }}">حذف</a></td>
-                </tr>
-            @endforeach
-            </tbody>
-        </table>
+            <div class="table-responsive-md">
+                <table class="table p-5 table-dark text-center table-hover">
+                    <thead>
+                    <tr>
+                        <th scope="col" width="1">#</th>
+                        <th scope="col">محصول</th>
+                        <th scope="col">قطعه آماده</th>
+                        <th scope="col">قیمت</th>
+                        <th scope="col">تاریخ</th>
+                        <th scope="col"></th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    @foreach($orders as $order)
+                        <tr>
+                            <th scope="row" title="{{ $order->id }}">{{ $loop->iteration }}</th>
+                            <td>{{ $order->humanized_product }}</td>
+                            <td>{{ $order->type ? 'بله' : '-' }}</td>
+                            <td>{{ $order->humanized_price }}</td>
+                            <td>{{ $order->jalali_date }}</td>
+                            <td><a class="btn btn-outline-danger btn-sm delete" href="{{ route('orders.delete', $order) }}">حذف</a></td>
+                        </tr>
+                    @endforeach
+                    </tbody>
+                </table>
+            </div>
     </div>
 @endsection
