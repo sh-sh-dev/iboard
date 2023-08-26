57a100ef5cccf763aa8b3d23a8fc4f5eb4d2e555
Shaygan <shayganshok0@gmail.com>
2023-08-26 16:54:28 +0330

    style(analytics): minor code-style changes

--- app/Http/Controllers/AnalyticsController.php
+++ app/Http/Controllers/AnalyticsController.php
@@ -4,31 +4,41 @@ namespace App\Http\Controllers;
 
 use App\Models\Order;
 use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Support\Facades\DB;
 
 class AnalyticsController extends BaseController
 {
     public function index(): \Illuminate\Contracts\View\View
     {
+        // @TODO: reduce queries
+        return view('analytics', [
+            'cards' => $this->getCardsData(),
+            'orders' => $this->getProductsAveragePrices(),
+        ]);
+    }
+
+    private function getCardsData(): array
+    {
         $orders = Order::query()
             ->select('price')
             ->get();
 
-        $averagePrices = $this->getProductsAveragePrices();
-
-        return view('analytics', [
-            'cards' => [
-                'count' => $orders->count(),
-                'sum' => $this->formatPrice($orders->sum('price')),
-                'avg' => $this->formatPrice($orders->avg('price')),
-            ],
-            'orders' => $averagePrices,
-        ]);
+        return [
+            'count' => $orders->count(),
+            'sum' => $this->formatPrice($orders->sum('price')),
+            'avg' => $this->formatPrice($orders->avg('price')),
+        ];
     }
 
     public function getProductsAveragePrices(): \Illuminate\Database\Eloquent\Collection
     {
         return Order::query()
-            ->selectRaw('product, type, AVG(price) as average_price, COUNT(*) as repeat_count')
+            ->select([
+                'product',
+                'type',
+                DB::raw('AVG(`price`) as average_price'),
+                DB::raw('COUNT(`id`) as repeat_count'),
+            ])
             ->groupBy('product', 'type')
             ->orderBy('product')
             ->get();
